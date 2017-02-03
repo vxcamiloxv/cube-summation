4b51bfed7143755666cb00fb7363d48677cb1e6e
Camilo Quimbayo <vxcamiloxv@openmailbox.org>
2017-02-03 13:02:45 -0500

    cube summation logic

---
+++ app/Http/Controllers/CubeController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Cubes;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Validator;
+
+class CubeController extends Controller
+{
+    public function command(Request $request)
+    {
+        $request->flash();
+        $rules = [
+            'testcase' => 'required|numeric|max:255'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        $cube = new Cubes($request->testcase, $request->operations);
+        $matrix = $cube->getMatrix();
+
+        if ($cube->step() > 0) {
+            $rules['matrix'] = 'required|numeric|max:255';
+            $rules['operations'] = 'required|numeric|max:255';
+            $validator = Validator::make($request->all(), $rules);
+        }
+
+        if ($cube->step() > 1) {
+            $rules['command_type'] = 'required|max:255';
+            $rules['command_value'] = 'required|max:255';
+            $validator = Validator::make($request->all(), $rules);
+
+            if (!$cube->operations()) {
+                $cube->testCase();
+                $cube->step(1);
+            }
+        }
+
+        if (!$matrix && $request->matrix) {
+            $matrix = $cube->createMatrix($request->matrix);
+        }
+
+        $type = $request->command_type;
+        $values = !empty($request->command_value) ? $request->command_value : "";
+        $values = preg_split("/[\s,]+/", $values);
+
+        $cube->step($request->testcase ? ($request->matrix && $request->operations ? 2 : 1) : 0);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+
+        } else if (strtoupper($type) === 'UPDATE') {
+            return $this->update($values, $cube, $validator);
+
+        } else if (strtoupper($type) == 'QUERY') {
+            return $this->query($values, $cube, $validator);
+
+        } else {
+            return view('home', [
+                'step' => $cube->step()
+            ]);
+        }
+    }
+
+    private function update($values, $cube, $validator)
+    {
+        try {
+            $results = session('results', []);
+            $cube->argCount(6, $values);
+            $cube->operations();
+
+            $x = $values[0]-1;
+            $y = $values[1]-1;
+            $z = $values[2]-1;
+            $w = $values[3];
+
+            #$cube->updateMatrix($x, $y, $x, $w);
+            array_push($results, ['value' => "OK", 'date' => date('Y-m-d H:i:s') ]);
+            session(['results' => $results]);
+
+            return view('home', [
+                'step' => $cube->step(),
+                'success' => TRUE,
+                'results' => $results
+            ]);
+        } catch(\Exception $error) {
+            $validator->errors()->add('command_value', $error->getMessage());
+            $cube->results = [];
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+    }
+
+    private function query($values, $cube, $validator)
+    {
+        $cube->operations();
+        return view('home', [
+            'step' => $cube->step("next")
+        ]);
+    }
+}
